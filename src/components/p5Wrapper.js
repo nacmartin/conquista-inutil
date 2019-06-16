368ea6478a53608a348f49e364c1804dbb887510
Nacho Martín <nacho@limenius.com>
2019-06-16 21:25:38 +0200

    Mouse move

---
+++ src/components/p5Wrapper.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import ace from "brace";
+require("brace/mode/javascript");
+require("brace/theme/textmate");
+
+const runSketch = (code, title) => {
+  const iframeToRemove = document.getElementById("iframe-wrapper-" + title)
+    .firstChild;
+  if (iframeToRemove) {
+    document
+      .getElementById("iframe-wrapper-" + title)
+      .removeChild(iframeToRemove);
+  }
+  const iframe = document.createElement("iframe");
+  iframe.id = title;
+  iframe.width = "100%";
+  iframe.height = "200";
+  iframe.frameBorder = "0";
+  iframe.marginHeight = "0";
+  iframe.marginWidth = "0";
+  iframe.title = title;
+  document.getElementById("iframe-wrapper-" + title).appendChild(iframe);
+
+  const p5Script = document
+    .getElementById(title)
+    .contentWindow.document.createElement("script");
+  p5Script.src = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.js";
+  document
+    .getElementById(title)
+    .contentWindow.document.body.appendChild(p5Script);
+  const userScript = document
+    .getElementById(title)
+    .contentWindow.document.createElement("script");
+  userScript.type = "text/javascript";
+  userScript.text = code;
+  userScript.async = false;
+  document
+    .getElementById(title)
+    .contentWindow.document.body.appendChild(userScript);
+};
+
+export class P5Wrapper extends Component {
+  componentDidMount() {
+    const editor = ace.edit("javascript-editor" + this.props.title);
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.setTheme("ace/theme/textmate");
+    editor.getSession().setTabSize(2);
+    runSketch(this.props.jsSource, this.props.title);
+
+    document
+      .getElementById("run-" + this.props.title)
+      .addEventListener("click", () => {
+        const code = editor.getValue();
+        runSketch(code, this.props.title);
+      });
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return (
+      <>
+        <div id={"iframe-wrapper-" + this.props.title}></div>
+        <button id={"run-" + this.props.title}>Run</button>
+        <div
+          id={"javascript-editor" + this.props.title}
+          style={{ height: "300px", width: "100%" }}
+          dangerouslySetInnerHTML={{ __html: this.props.jsSource }}
+        ></div>
+      </>
+    );
+  }
+}
--- src/pages/noise.js
+++ src/pages/noise.js
@@ -1,4 +1,26 @@
-import React from "react"
-import Link from "gatsby-link"
+import React from "react";
+import { P5Wrapper } from "../components/p5Wrapper";
+import noise01 from "raw-loader!./sketches/noise01.p5";
+import mouse from "raw-loader!./sketches/mouse.p5";
 
-export default () => <div></div>
+export default class Noise extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ctx = { x: 10 };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.ctx.x = this.ctx.x + 10;
+    }, 100);
+  }
+
+  render() {
+    return (
+      <div style={{ width: "500px", margin: "0 auto" }}>
+        <P5Wrapper jsSource={noise01} title="noise01" />
+        <P5Wrapper jsSource={mouse} title="mouse" />
+      </div>
+    );
+  }
+}
