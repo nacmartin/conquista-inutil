6a49d5313afe98fe46e6c8a7f63d7e0c704efe57
Nacho Martín <nacho@limenius.com>
2019-06-16 22:06:36 +0200

    Example with pixel density

--- src/pages/noise.js
+++ src/pages/noise.js
@@ -4,22 +4,11 @@ import noise01 from "raw-loader!./sketches/noise01.p5";
 import mouse from "raw-loader!./sketches/mouse.p5";
 
 export default class Noise extends React.Component {
-  constructor(props) {
-    super(props);
-    this.ctx = { x: 10 };
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      this.ctx.x = this.ctx.x + 10;
-    }, 100);
-  }
-
   render() {
     return (
       <div style={{ width: "500px", margin: "0 auto" }}>
         <P5Wrapper jsSource={noise01} title="noise01" />
-        <P5Wrapper jsSource={mouse} title="mouse" />
+        {/*<P5Wrapper jsSource={mouse} title="mouse" />*/}
       </div>
     );
   }
